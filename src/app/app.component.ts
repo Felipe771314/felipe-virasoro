dab8f2edc1d91ab8807473cacfcff8d5b2ac52d5
Felipe Virasoro <felipe.virasoro.contractor@bbva.com>
2024-03-02 16:48:44 +0100

    new proyect for my

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../app/components/header/header.component';
+import { FooterComponent } from '../app/components/footer/footer.component';
+import { BtnWhatsappComponent } from '../app/components/btn-whatsapp/btn-whatsapp.component';
+import { SiderComponent } from './components/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,BtnWhatsappComponent,SiderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/btn-whatsapp/btn-whatsapp.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-btn-whatsapp',
+  standalone: true,
+  imports: [],
+  templateUrl: './btn-whatsapp.component.html',
+  styleUrls: ['./btn-whatsapp.component.sass']
+})
+export class BtnWhatsappComponent {
+
+}
